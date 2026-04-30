a79e32f49675f3795578c97f3963e594fe51b33f
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-30 13:19:16 +0300

    style buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 function App() {
   return (
-    <>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo quis
-      quo possimus harum nemo sequi qui, nostrum eos nam rem a vitae facere
-      inventore! Accusantium fugiat id, deserunt assumenda illo ab laudantium
-      repellat ad est ex cumque nam minus corrupti sunt mollitia praesentium cum
-      dolorum! Velit optio placeat recusandae sunt?
-    </>
+    <body className="dark-mode">
+      <button className="btn btn--primary btn--filter active">All</button>
+      <button className="btn btn--primary btn--remove">Remove</button>
+      <button className="btn btn--theme-toggle is-dar"></button>
+      <button className="btn btn--extension-toggle active"></button>
+    </body>
   );
 }
 
